38e971a89238f651a1303d9811e52c66ccccee8c
Lotito30 <112903235+Lotito30@users.noreply.github.com>
2022-09-12 23:57:14 -0300

    Finished homework JSIII

---
+++ 04-JS-III/homework/conceptos.js
@@ -0,0 +1,8 @@
+Concepto array
+
+array: Es una lista que puede almacenar cualquier tipo de cadena/strings, booleanos, numeros, undifined, null, otro array. Sirve para almacenar varios
+	   valores los cuales se vayan a usar en un momento dado, se recorre el array con un for para obtener todos su valores, obtienes su indice con 
+	   nombreArray[0 || 1 || 2] entre corchetes el numero de indice que quieras obtener, puedes eliminar el ultimo valor de un array con nombreArray.pop(), agregar 
+	   un valor nuevo en el ultimo indice con nombreArray.push(valor), agregar un nuevo valor en el indice[0] del array con nombreArray.unshift(valor), eliminar 
+	   un valor del indice[0] del array con nombreArray.shift(), obtienes la longitud de un array con nombreArray.length, puedes asignar un nuevo valor en el
+	   indice que ya existe con nombreArray[0 || 1 || 2] = valor 
